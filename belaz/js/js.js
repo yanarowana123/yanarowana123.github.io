939e3cb13770d24ea13761951b0a374cd624f72a
Bayan Amangeldin <yanarowana123@gmail.com>
2019-09-15 20:05:49 +0600

    Update js.js

--- belaz/js/js.js
+++ belaz/js/js.js
@@ -1,6 +1,6 @@
 let questions = document.querySelector('.faq-list');
 
-if(faq!==null){
+if(questions!==null){
 	questions.addEventListener('click', function(e) {
     if (e.target.classList.contains('faq-list__text')) {
         e.target.parentNode.querySelector('.faq-list__answer').classList.toggle('block');

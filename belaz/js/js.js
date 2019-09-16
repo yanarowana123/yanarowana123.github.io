97811f4e053703c7ee1f43d11e79ac3a5b599b9e
Bayan Amangeldin <yanarowana123@gmail.com>
2019-09-16 12:41:08 +0600

    modal

--- belaz/js/js.js
+++ belaz/js/js.js
@@ -1,10 +1,32 @@
 let questions = document.querySelector('.faq-list');
+let modal = document.querySelector('.modal');
+let modalBtn = document.querySelector('.header-phone-btn');
+let modalClose = document.querySelector('.modal__close');
 
-if(questions!==null){
-	questions.addEventListener('click', function(e) {
-    if (e.target.classList.contains('faq-list__text')) {
-        e.target.parentNode.querySelector('.faq-list__answer').classList.toggle('block');
+modalBtn.addEventListener('click', function() {
+    modal.style.display = 'block';
+})
+
+modalClose.addEventListener('click', function() {
+    modal.style.display = "none";
+})
+
+window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
 })
 
-}
+questions.addEventListener('click', function(e) {
+    if (e.target.classList.contains('faq-list__text')) {
+        if (e.target.parentNode.querySelector('.faq-list__answer').style.maxHeight) {
+            e.target.parentNode.querySelector('.faq-list__answer').style.maxHeight = null;
+            e.target.parentNode.querySelector('.faq-list__answer').classList.toggle('block');
+        } else {
+            e.target.parentNode.querySelector('.faq-list__answer').classList.toggle('block');
+            e.target.parentNode.querySelector('.faq-list__answer').style.maxHeight = e.target.parentNode.querySelector('.faq-list__answer').scrollHeight + 'px';
+        }
+
+
+    }
+})

634655cb6775138e5939af98a8342c217f690957
Bayan Amangeldin <yanarowana123@gmail.com>
2019-10-20 15:28:29 +0600

    qwe

--- toyota/js/main.js
+++ toyota/js/main.js
@@ -1,8 +1,10 @@
 $(function() {
+  
 
     $('.slider-wrapper').on('init reInit',function(event,slick){
         var amount = slick.slideCount;
         $('#range').attr('max',amount);
+   
       })
       
       $('.slider-wrapper').on('afterChange',function(e,slick,currentSlide){
@@ -13,13 +15,14 @@ $(function() {
         $('.slider-wrapper').slick('slickGoTo',this.value-1);
       });
 
+    
     $('.slider-wrapper').slick({
      centerMode: true,
         // centerPadding: '235px',
         centerPadding: '0px',
 
         // slidesToShow: 2.35,
-        slidesToShow: 2.05,
+        slidesToShow: 3,
         slidesToScroll:1,
         arrows: false, 
         dots:false,
@@ -32,8 +35,9 @@ $(function() {
         infinity: true,
         responsive: [
           {
-            breakpoint: 900,
+            breakpoint: 450,
             settings:{
+              slidesToShow:1,
               centerPadding: '0px'
               
             }
@@ -42,6 +46,40 @@ $(function() {
       
     
     })
+
+    let yaris = $('.yaris');
+    let corolla = $('.corolla');
+    let chr = $('.c-hr');
+    let chr2000 = $('.c-hr');
+    let price = $('.header-price__text');
+    let title = $('.about__title');
+    let description = $('.about__text');
+    
+    $('.slider-wrapper').on('afterChange',function(e,slick,currentSlide){
+      let x =$('.slick-center');
+      let y =x.children().children();
+      console.log(y);
+      if(y.hasClass('yaris')){
+        price.text('От 699 шекелей в месяц');
+        title.text('Toyota Yaris Hibrid');
+        description.text('Этот автомобиль заряжает энергией города! Он тихий и умный, легкий и надежный! Путешествуйте по городу на электричестве до 50% всего времени в пути без подзарядки — начните ездить по-новому! ')
+      }
+      else if(y.hasClass('c-hr')){
+        price.text('От 1199 в месяц');
+        title.text('Toyota C-HR Hibrid');
+        description.text('Оригинальный, яркий и смелый кроссовер Toyota C-HR Hybrid, безусловный «хит» на дорогах Израиля. Toyota C-HR построен на основе уникальной модульной архитектуры TNGA — это новейший подход инженеров Toyota, позволяющий создавать более комфортные, управляемые и безопасные автомобили с ярким, экспрессивным дизайном.')
+      }
+      else if(y.hasClass('corolla')){
+        price.text('От 1299 шекелей в месяц');
+        title.text('Toyota Corolla Hibrid');
+        description.text('Стремительный дизайн, уникальные для своего класса опции, высокий уровень исполнения, а также азартная динамика и управляемость, делают Toyota Corolla Hibrid лучшим седаном в своем классе.')
+      } else{
+        price.text('Цену новинки уточняйте у наших операторов.');
+        title.text('Toyota C-HR Hibrid 2020');
+        description.text('Еще ярче, еще лучше и еще технологичнее! Топ-версия Toyota C-HR Hibrid 2020 будет выдавать показатель в 184 лошадиных силы, а также отличаться более спортивными настройками подвески. Изменения коснуться внешнего вида, усовершенствована медиасистема с 8-дюймовым экраном, серьезно улучшена шумоизоляция. Первые автомобили уже в пути!')
+      }
+    })
+    
 
     
 });

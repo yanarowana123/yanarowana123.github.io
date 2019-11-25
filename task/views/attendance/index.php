6618cecabb94412847f36ceca0abc05543db934f
Bayan Amangeldin <yanarowana123@gmail.com>
2019-11-25 23:16:35 +0600

    q

---
+++ task/views/attendance/index.php
@@ -0,0 +1,7 @@
+<?
+use yii\helpers\Html;
+$months = array('Январь' , 'Февраль' , 'Март' , 'Апрель' , 'Май' , 'Июнь' , 'Июль' , 'Август' , 'Сентябрь' , 'Октябрь' , 'Ноябрь' , 'Декабрь' );
+
+foreach ($months as $key=>$month){
+echo Html::a($month, ['attendance/show', 'month' => $key+1], ['class' => 'btn btn-success']) ;
+}
